4020757985b07da2dc493e05a256ac84c2cd24d5
Hayden Cheers <hayden.cheer@me.com>
2020-01-31 12:53:58 +1100

    progress

--- testconsole/src/main/kotlin/jcinterpret/testconsole/plagiariser/SPPlagiariseWrapperApplication.kt
+++ testconsole/src/main/kotlin/jcinterpret/testconsole/plagiariser/SPPlagiariseWrapperApplication.kt
@@ -9,37 +9,6 @@ import kotlin.streams.toList
 
 object SPPlagiariseWrapperApplication {
 
-//    val completed = arrayOf(
-//        "SORT_BUBBLE_1",
-//        "SORT_QUICKSORT_1",
-//        "STRINGMATCH_RK_2",
-//        "CHANGEMAKING_REC_1",
-//        "CHANGEMAKING_DP_2",
-//        "STRINGMATCH_KMP_1",
-//        "STRINGMATCH_BM_2",
-//        "CHANGEMAKING_ITER_2",
-//        "MST_KRUSKALS_1",
-//        "CHANGEMAKING_DP_1",
-//        "CHANGEMAKING_ITER_1",
-//        "CHANGEMAKING_REC_2",
-//        "MST_KRUSKALS_2",
-//        "MST_KRUSKALS_3",
-//        "MST_PRIMMS_1",
-//        "MST_PRIMMS_2",
-//        "MST_PRIMMS_3",
-//        "MST_REVERSEDELETE_1",
-//        "MST_REVERSEDELETE_2",
-//        "SORT_BUBBLE_2",
-//        "SORT_MERGE_1",
-//        "SORT_MERGE_2",
-//        "SORT_QUICKSORT_2",
-//        "SORT_QUICKSORT_3",
-//        "SORT_QUICKSORT_4",
-//        "STRINGMATCH_BM_1",
-//        "STRINGMATCH_KMP_4",
-//        "STRINGMATCH_RK_1"
-//    )
-
     @JvmStatic
     fun main(args: Array<String>) {
         val input = Paths.get(args[0])
